81bf3f63fce192048f3a03270c7df9622e46ecb6
Dang Viet Son <dang.viet.son.hp@gmail.com>
2016-02-11 22:16:11 +0700

    Facebook plugin installed

---
+++ wp-content/plugins/simple-facebook-twitter-widget/js/simple-facebook-page-root.js
@@ -0,0 +1,11 @@
+(function (d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement(s);
+	js.id = id;
+	js.async = true;
+	js.src = "//connect.facebook.net/" + sfpp_script_vars.language + "/sdk.js#xfbml=1&version=v2.3";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
